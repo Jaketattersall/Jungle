2902d3380d4bceda2c2c83c41993f461ed3a7c7b
jaketattersall <jaketattersall90@gmail.com>
2024-04-11 10:23:22 -0700

    build authenticate credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,10 +7,9 @@ class SessionsController < ApplicationController
   
     # Create 'create' action for SessionsController
     def create
-        user = User.find_by_email(params[:email])
+           # If user is successfully authenticated
+    if user = User.authenticate_with_credentials(params[:email], params[:password])
 
-        # If user exists and password entered is correct
-        if user && user.authenticate(params[:password])
     
           # Save user id as cookie so they stay logged in
           session[:user_id] = user.id
