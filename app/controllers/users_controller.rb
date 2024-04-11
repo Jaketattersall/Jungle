337edf17149b5914ed16f731e3c4f4fa5c3a5462
jaketattersall <jaketattersall90@gmail.com>
2024-04-10 17:29:08 -0700

    create user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+     # Create 'new' action for UsersController
+  def new
+  end
+
+  # Create 'create' action for UsersController
+  def create  
+  end
+end
